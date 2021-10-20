7d7f5e6c45db9ce4a8ea2552441702b611accd2c
SlothFox <alexander.rehmer@gmail.com>
2021-10-20 14:19:40 +0200

    stuff

--- sandbox/ECC22_InitEvaluation.py
+++ sandbox/ECC22_InitEvaluation.py
@@ -47,10 +47,14 @@ res=pkl.load(open(path+file,'rb'))
 #             res.drop(res.index[i],inplace = True)
 #     except:
 #         break
-        
+     
+plt.close('all')
+    
 palette = sns.color_palette()
 
-fig, axs = plt.subplots() #plt.subplots(2,gridspec_kw={'height_ratios': [1, 1.5]})
+# fig, axs = plt.subplots() #plt.subplots(2,gridspec_kw={'height_ratios': [1, 1.5]})
+
+fig, axs = plt.subplots(1,3,gridspec_kw={'width_ratios': [1.5, 1.5, 1.5]})
 
 fig.set_size_inches((9/2.54,4/2.54))
 # fig.set_size_inches((9,4))
@@ -58,18 +62,55 @@ fig.set_size_inches((9/2.54,4/2.54))
 # sns.boxplot(x='dim_phi', y='BFR_test', hue='structure', data=res, ax=axs,
 #             color=".8", linewidth=2)
 
-sns.stripplot(x='dim_phi', y='BFR_test', hue='structure',data=res, 
-                  palette=palette, ax=axs, linewidth=0.1,
-                   dodge=True,zorder=1,size=5)
+sns.stripplot(x='structure', y='BFR_test', data=res[res['dim_phi']==1], 
+                  palette=palette, ax=axs[0], linewidth=0.3,
+                   dodge=True,zorder=1,size=3,marker='x')
+sns.stripplot(x='structure', y='BFR_test', data=res[res['dim_phi']==2], 
+                  palette=palette, ax=axs[1], linewidth=0.3,
+                   dodge=True,zorder=1,size=3,marker='x')
+sns.stripplot(x='structure', y='BFR_test', data=res[res['dim_phi']==5], 
+                  palette=palette, ax=axs[2], linewidth=0.3,
+                   dodge=True,zorder=1,size=3,marker='x')
+# sns.stripplot(x='structure', y='BFR_test', hue='dim_phi',data=res, 
+#                   palette=palette, ax=axs, linewidth=0.1,
+#                    dodge=True,zorder=1,size=5)
 
 # sns.boxplot(x='theta', y='BFR', hue='model',data=BFR_on_val_data, 
 #                   palette="Set1",fliersize=2,ax=axs[1], linewidth=1)
 
-axs.legend_.remove()
-axs.set_xlabel(r'$\dim(\phi_k)$')
-axs.set_ylabel(None)
-axs.set_ylim(55,105)
-# fig.tight_layout()
+# axs.legend_.remove()
+# axs.set_xlabel(r'$\dim(\phi_k)$')
+
+
+axs[0].set_title(r'$\dim(\phi_k)=1$',fontsize=10)
+axs[1].set_title(r'$\dim(\phi_k)=2$',fontsize=10)
+axs[2].set_title(r'$\dim(\phi_k)=5$',fontsize=10)
+
+axs[0].set_xlabel(None)
+axs[1].set_xlabel(None)
+axs[2].set_xlabel(None)
+
+axs[0].set_ylabel(None)
+axs[1].set_ylabel(None)
+axs[2].set_ylabel(None)
+
+xticklabels = [r'$a_{11}$',r'$a_{12}$',r'$a_{13}$',r'$a_{21}$',r'$a_{22}$',
+                        r'$a_{23}$',r'$a_{31}$',r'$a_{32}$',r'$a_{33}$']
+
+fontsize = 8
+
+axs[0].set_xticklabels(xticklabels,fontsize=fontsize)
+axs[2].set_xticklabels(xticklabels,fontsize=fontsize)
+axs[1].set_xticklabels(xticklabels,fontsize=fontsize)
+
+axs[2].set_yticklabels([])
+axs[1].set_yticklabels([])
+
+
+axs[0].set_ylim(55,105)
+axs[1].set_ylim(55,105)
+axs[2].set_ylim(55,105)
+fig.tight_layout()
 
 
 fig.savefig('MSD_ModelSelection.png', bbox_inches='tight',dpi=600)
--- sandbox/MSD_LuGre_Identification.py
+++ sandbox/MSD_LuGre_Identification.py
@@ -12,7 +12,7 @@ from optim import param_optim
 
 ''' User specified parameters '''
 dim_x = 3
-inits = 20
+inits = 10
 lamb = 0.01
 
 ''' Data Preprocessing '''
@@ -68,11 +68,12 @@ data['x_train'] = x_LS.reshape(1,-1,dim_x)
 # p_opts = {"expand":False}
 s_opts = {"max_iter": 1000, "print_level":0, 'hessian_approximation': 'limited-memory'}
 
-dim_thetaA = [1]
+dim_thetaA = [2]
 # A_0s = [np.identity(9)[i,:].reshape((dim_x,dim_x)) for i in range(0,dim_x**2)]
-A_0s = [np.array([[1,1,1],[0,0,0],[0,0,0]])]
-activations = [ [[1,1,1,0]] ]      
-dim_phis = [1,2,5]
+A_0s = [np.array([[1,0,0],[0,0,0],[0,0,0]])]
+A_1 = np.array([[0,1,0],[0,0,0],[0,0,0]])
+activations = [ [[1,1,1,0], [1,1,1,0]] ]      
+dim_phis = [5]
 
 counter = 0
 
@@ -82,14 +83,15 @@ for dimA in dim_thetaA:
             for dim_phi in dim_phis:
     
                 model = NN.Rehmer_NN_LPV(dim_u=1,dim_x=dim_x,dim_y=1,
-                                         dim_thetaA=dimA, NN_A_dim=[[5,dim_phi,5,1]],
+                        dim_thetaA=dimA, NN_A_dim=[[5,dim_phi,5,1],[5,dim_phi,5,1]],
                                          NN_A_act=activation)
                 
-                model.FrozenParameters = ['A','B','C','A_0']
+                model.FrozenParameters = ['A','B','C','A_0','A_1']
                 model.InitialParameters = initial_params = {'A': LSS['A'][0][0],
                                                             'B': LSS['B'][0][0],
                                                             'C': LSS['C'][0][0],
-                                                            'A_0':A_0}
+                                                            'A_0':A_0,
+                                                            'A_1':A_1}
                 
                 results_new = param_optim.ModelTraining(model,data,inits,
                                          p_opts=None,s_opts=s_opts)
@@ -113,7 +115,7 @@ for dimA in dim_thetaA:
                 
                 counter = counter + 1
    
-pkl.dump(results,open('./Results/MSD/MSD_LPVNN_3states_111_'+
+pkl.dump(results,open('./Results/MSD/MSD_LPVNN_3states_2theta_111_'+
                                           'lam'+str(lamb)
                                           +'.pkl','wb'))
 
